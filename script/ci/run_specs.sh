183c33f92a6fab94df190c83d9105278ed7fe875
Matt Ensor <matt.ensor@redbubble.com>
2016-09-04 09:34:50 +1000

    spec script

---
+++ script/ci/run_specs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "-----------------------------"
+echo "-------- CyclelifeHQ --------"
+echo "----------- Specs -----------"
+echo "-----------------------------"
+
+set -e
+
+echo "----- USER -----"
+echo `whoami`
+echo "----- Current DIR -----"
+echo `pwd`
+echo "----- WHICH RUBY -----"
+echo `which ruby`
+
+
+echo "--- bundle install ---"
+bundle install
+
+# if ! ruby -v &> /dev/null; then
+#   rbenv update
+#   rbenv install `cat .ruby-version`
+# fi
+#
+#
+# npm install
+#
+# bundle exec rake db:drop || true
+# bundle exec rake db:create
+# bundle exec rake db:migrate
+#
+# bundle exec rspec spec
